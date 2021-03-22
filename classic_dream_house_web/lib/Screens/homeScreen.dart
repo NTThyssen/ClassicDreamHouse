b056d588bc699d31087b25375957ad01e7d7a7e3
futte111 <ntthyssen@gmail.com>
2021-03-22 22:49:23 +0100

    main page for web

--- classic_dream_house_web/lib/Screens/homeScreen.dart
+++ classic_dream_house_web/lib/Screens/homeScreen.dart
@@ -1,4 +1,5 @@
 import 'package:classic_dream_house_web/Widgets/projectWidget.dart';
+import 'package:classic_dream_house_web/theme.dart';
 import 'package:flutter/material.dart';
 import 'package:sticky_headers/sticky_headers/widget.dart';
 
@@ -8,85 +9,203 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  int pageIndex = 0;
+  PageController pageController = PageController(initialPage:0);
+
+
+
+
+
+  @override
+  void dispose() {
+    pageController.dispose();
+    super.dispose();
+  }
+  @override
+  void initState() {
+    pageController = new PageController();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: appTheme.primaryColor,
         title: Text("Classic Dream House"),
         centerTitle: true,
       ),
-      body: LayoutBuilder(
-          builder: (BuildContext context, BoxConstraints constraints) {
-            return Container(
+      body: Column(
+        children: [
+          Spacer(flex: 1,),
+          Center(
+            child: Container(
               decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                      begin: Alignment.topRight,
-                      end: Alignment.bottomLeft,
-                      stops: [
-                        0.1,
-                        0.6
-                      ],
-                      colors: [
-                        Colors.white,
-                        Colors.white70
-                      ])),
-              child: Center(
-                child: Container(
-                  decoration: BoxDecoration(
-                    border: Border.all(color: Colors.grey),
-                  ),
-                  width: MediaQuery.of(context).size.width*0.80,
-                  child: ListView.builder(
-                    itemCount: 1,
-                    itemBuilder: (BuildContext context, int index){
-                      return StickyHeader(
-                        header: Container(
-                           color: Colors.blue.withOpacity(0.7),
-                          width: MediaQuery.of(context).size.width*0.80,
-                          child: Padding(
-                            padding: EdgeInsets.all(16),
-                            child: Wrap(
-                              spacing: 50,
-                              children: [
-                                Text("Hjem", style: TextStyle(color: Colors.white),),
-                                Text("Oversigt", style: TextStyle(color: Colors.white),),
-                                Text("Indstillinger", style: TextStyle(color: Colors.white))
-                              ],
-                            ),
-                          ),
-                        ),
-
-                        content: Container(
-                          width: MediaQuery.of(context).size.width*0.80,
-                          color: Colors.transparent.withOpacity(0.5),
-                          child: Padding(
-                            padding: const EdgeInsets.all(16.0),
-                            child: Wrap(
-                              alignment: WrapAlignment.spaceEvenly,
-                              spacing: 100,
-                              runSpacing: 40,
-                              children: [
-                                ProjectWidget(),
-                                ProjectWidget(),
-                                ProjectWidget(),
-                                ProjectWidget(),
-                                ProjectWidget(),
-                                ProjectWidget(),
-                                ProjectWidget(),
-                                ProjectWidget(),
-                                ProjectWidget(),
-                              ],
-                            ),
-                          ),
-                        ),
-                      );
-                    }
-
-                    ),
+                borderRadius: BorderRadius.all(Radius.circular(5)),
+                color:  appTheme.primaryColor,
+              ),
+              width: MediaQuery.of(context).size.width*0.85,
+              child: Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: Wrap(
+                  spacing: 50,
+                  children: [
+                    TextButton(child:Text("Hjem"),  onPressed: () {  },),
+                    TextButton(child:Text("Oversigt"),  onPressed: () {  },),
+                    TextButton(child:Text("Indstillinger"),
+                        onPressed: () {  },)
+                  ],
                 ),
               ),
-            );
-          }),
-    );
-  }
+            ),
+          ),
+              Expanded(
+                flex: 20,
+                child: Stack(
+                  children: [
+                    Positioned(
+                      top: 10,
+                      bottom: 120,
+                      left: MediaQuery.of(context).size.width*0.12,
+                      right: MediaQuery.of(context).size.width*0.12,
+                      child: Container(
+                      width: MediaQuery.of(context).size.width*0.60,
+                      height: MediaQuery.of(context).size.height*0.70,
+                      color: appTheme.dividerColor,
+                      ),
+                    ),
+                    PageView(
+                      physics: AlwaysScrollableScrollPhysics() ,
+                      controller: pageController,
+                    children: [
+                      Scrollbar(
+                        controller: ScrollController(),
+                        child: SingleChildScrollView(
+                          child: Padding(
+                           padding: EdgeInsets.all(16),
+                           child: Container(
+                             width: MediaQuery.of(context).size.width*0.95,
+                             child: Padding(
+                               padding: const EdgeInsets.all(16.0),
+                               child: Wrap(
+                                 alignment: WrapAlignment.spaceEvenly,
+                                 spacing: 100,
+                                 runSpacing: 40,
+                                 children: [
+                                   ProjectWidget(),
+                                   ProjectWidget(),
+                                   ProjectWidget(),
+                                   ProjectWidget(),
+                                   ProjectWidget(),
+                                   ProjectWidget(),
+                                 ],
+                               ),
+                             ),
+                           ),
+                          ),
+                        ),
+                      ),
+                      Scrollbar(
+                        controller: ScrollController(),
+                        child: SingleChildScrollView(
+                          child: Padding(
+                            padding: EdgeInsets.all(16),
+                            child: Container(
+                              width: MediaQuery.of(context).size.width*0.95,
+                              child: Padding(
+                                padding: const EdgeInsets.all(16.0),
+                                child: Wrap(
+                                  alignment: WrapAlignment.spaceEvenly,
+                                  spacing: 100,
+                                  runSpacing: 40,
+                                  children: [
+                                    ProjectWidget(),
+                                    ProjectWidget(),
+                                    ProjectWidget(),
+                                    ProjectWidget(),
+                                    ProjectWidget(),
+                                    ProjectWidget(),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                   ] ),
+                    Positioned(
+                      right: 40,
+                      top: MediaQuery.of(context).size.height*0.32 ,
+                      child: MaterialButton(
+                      height: 50,
+                        minWidth: 10,
+                        color: appTheme.primaryColor,
+                        shape: CircleBorder(),
+                        child: Icon(Icons.arrow_forward_ios_outlined, color: Colors.white), onPressed: (){
+                        pageController.nextPage(duration: Duration(milliseconds: 1000), curve: Curves.ease);
+                        pageIndex++;
+                        if(pageIndex > 0){
+                          setState(() {
+
+                          });
+                        }
+                      }
+                        )
+                    ),
+                    Positioned(
+                        left: 40,
+                        top: MediaQuery.of(context).size.height*0.32 ,
+                        child: MaterialButton(
+                            height: 50,
+                            minWidth: 10,
+                            color: pageIndex == 0  ? appTheme.disabledColor : appTheme.primaryColor,
+                            shape: CircleBorder(),
+                            child: Icon(Icons.arrow_back_ios_outlined, color: Colors.white), onPressed: (){
+                              pageController.previousPage(duration: Duration(milliseconds: 1000), curve: Curves.ease);
+                              pageIndex--;
+                              if(pageIndex == 0){
+                                setState(() {
+
+                                });
+                              }
+                        }
+                        )
+                    )
+                ]),
+              ),
+          Expanded(
+              flex: 3,
+              child: Align(
+                alignment: Alignment.center,
+                child: Container(
+                  width: 500,
+                  height: 100,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20)
+                  ),
+                  child: Padding(
+                    padding: EdgeInsets.fromLTRB(0, 0, 0, 50),
+                    child: ElevatedButton(
+                      style: ButtonStyle(
+                          backgroundColor: MaterialStateProperty.all<Color>(appTheme.primaryColor),
+                          shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                              RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(18.0),
+                              )
+                          )
+                      ),
+                      child: Text("Opret Nyt Bygge Projekt"),
+                      onPressed: () {  },
+
+          ),
+                  ),
+                ),
+              ),
+      )
+      ]),
+      );
+    }
+
+
 }
+
--- classic_dream_house_web/lib/Widgets/projectWidget.dart
+++ classic_dream_house_web/lib/Widgets/projectWidget.dart
@@ -1,3 +1,4 @@
+import 'package:classic_dream_house_web/theme.dart';
 import 'package:flutter/material.dart';
 
 class ProjectWidget extends StatefulWidget {
@@ -6,11 +7,12 @@ class ProjectWidget extends StatefulWidget {
 }
 
 class _ProjectWidgetState extends State<ProjectWidget> {
+  double progress = 0.10;
   @override
   Widget build(BuildContext context) {
     return Container(
         width: 400,
-        height: 400,
+        height: 300,
         decoration: BoxDecoration(
           boxShadow: [
             BoxShadow(
@@ -20,15 +22,83 @@ class _ProjectWidgetState extends State<ProjectWidget> {
               offset: Offset(0, 1), // changes position of shadow
             ),
           ],
-          border: Border.all(color: Colors.white),
+          border: Border.all(color: Colors.black),
           image: DecorationImage(
-            colorFilter: new ColorFilter.mode(Colors.transparent.withOpacity(0.2), BlendMode.dstATop),
+            colorFilter: new ColorFilter.mode(Colors.transparent.withOpacity(1), BlendMode.dstATop),
             image: AssetImage("images/funkHouse.jpg"),
             fit: BoxFit.cover,
           ),
         ),
-        child: Container(
-          child: Text("Kunde: Hans Hansen", style: TextStyle(color: Colors.white),),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Container(),
+            SizedBox(
+              height: 80,
+              width: 80,
+              child: Stack(
+                children: [
+                  SizedBox(
+                    height: 80,
+                    width: 80,
+                    child: CircularProgressIndicator(
+                      strokeWidth: 8,
+                      backgroundColor: appTheme.accentColor,
+                      valueColor: new AlwaysStoppedAnimation<Color>(appTheme.primaryColor),
+                      value: progress,
+                    ),
+                  ),
+                  Center(
+                      child: Text(progress.toString() + "%")
+                  )
+                ],
+              ),
+            ),
+            Container(
+              decoration: BoxDecoration(
+                color: Colors.white.withOpacity(0.92),
+                  borderRadius: BorderRadius.only(
+                      topLeft:  const  Radius.circular(10.0),
+                      topRight: const  Radius.circular(10.0))
+              ),
+              width: 400,
+              height: 100,
+              child:  Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Text("Navn: Hans Hansen", style: TextStyle(color: appTheme.primaryColor),),
+                    ),
+                      Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Text("Budget : 4 Mil", style: TextStyle(color: appTheme.primaryColor),),
+                      ),
+                    ],
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Text("Addresse: Kollegiebakken 9 ", style: TextStyle(color: appTheme.primaryColor),),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Text("Igang med: Fundament", style: TextStyle(color: appTheme.primaryColor),),
+                      ),
+                  ],
+                  ),
+
+                ],
+              ),
+            ),
+          ],
         )
       );
   }
