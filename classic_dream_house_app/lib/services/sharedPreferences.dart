c2416f900570f20b7f138db5c385ee58cf6c56f0
futte111 <ntthyssen@gmail.com>
2021-06-16 18:36:59 +0200

    non future

--- classic_dream_house_app/lib/services/sharedPreferences.dart
+++ classic_dream_house_app/lib/services/sharedPreferences.dart
@@ -2,21 +2,18 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'dart:convert';
 
 class SharedPref {
-
   read(String key) async {
-    Future<SharedPreferences> prefs = SharedPreferences.getInstance();
-    prefs.then((value) {
-      if(value.getString(key)==null){
-        return null;
-      }
-      else{
-        return json.decode(value.getString(key)) ;
-      }
-    } ) ;
+    final prefs = await SharedPreferences.getInstance();
+    if(prefs.getString(key) == null){
+      return null;
+    }else{
+      return json.decode(prefs.getString(key)) ;
+    }
+
   }
 
   save(String key, value) async {
-    Future<SharedPreferences> prefs = SharedPreferences.getInstance();
-    prefs.then((val) => val.setString(key, json.encode(value)) ) ;
+    final prefs = await SharedPreferences.getInstance();
+    prefs.setString(key, json.encode(value));
   }
 }
