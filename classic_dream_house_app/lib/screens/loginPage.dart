d045489ef419398c2a4e6921dab1e68889a197b0
futte111 <ntthyssen@gmail.com>
2021-06-22 20:48:52 +0200

    native alert

--- classic_dream_house_app/lib/screens/loginPage.dart
+++ classic_dream_house_app/lib/screens/loginPage.dart
@@ -101,29 +101,15 @@ class _LoginPageState extends State<LoginPage> {
                               icon: Icon(Icons.help, color: appTheme.primaryColor, size:  30,),
                               iconCupertino: Icon(Icons.help, color: appTheme.primaryColor, size: 30,),
                               onPressed: () async {
-                                  return await showDialog(
+                                await showOkAlertDialog(context: context, alertStyle: );
+                                  await showAlertDialog(
                                       context: context,
-                                      builder: (BuildContext context) {
-                                        return AlertDialog(
-                                          title: Container(width: 50, height: 50,child: Center(child: Text("Info", style: appTheme.textTheme.headline2.copyWith(color: appTheme.primaryColor),))),
-                                          content: Container(
-                                            height: 80,
-                                              width: 250,
-                                              child: Text("Vi skulle gerne have sendt dig mail med et ID, som du skal indtaste i App'en. Ellers kontakt os om ID",style: appTheme.textTheme.bodyText1.copyWith(color:appTheme.primaryColor, fontSize: 16),)),
-                                          actions: [
-                                            Container(
-                                              constraints: BoxConstraints(maxWidth: 95, maxHeight: 40),
-                                              child: FlatButton(
-                                                  child: Text("Ok"),
-                                                  onPressed: () {
-                                                    Navigator.pop(context);
-                                                  }),
-                                              width: 120,
-                                              height: 40,
-                                            )
-                                          ],
-                                        );
-                                      }
+                                      title: "Info",
+                                      actions: [
+                                        AlertDialogAction(key: Key("1"), label: "Ok")
+                                      ],
+                                      message: "Vi skulle gerne have sendt dig mail med et ID, som du skal indtaste i App'en. Ellers kontakt os om ID"
+
                                   );
 
                           },
